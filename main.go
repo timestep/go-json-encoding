2f3aa900610f45d787f2b3746443bad67df27f89
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-10-13 17:17:21 -0400

    json stuff

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Profile struct {
+	Name    string
+	Hobbies []string
+}
+
+func main() {
+	http.HandleFunc("/", ProfileHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	profile := Profile("Alex", []string{"snowboarding", "programming"})
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
